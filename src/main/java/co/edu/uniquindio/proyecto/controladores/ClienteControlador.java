f2fef15789d2b385bffdbb644ce95dc4a834d0d3
david velasco <david.santiago.velasco.12@gmail.com>
2024-04-05 21:38:05 -0500

    clase 5/04/24

---
+++ src/main/java/co/edu/uniquindio/proyecto/controladores/ClienteControlador.java
@@ -0,0 +1,168 @@
+package co.edu.uniquindio.proyecto.controladores;
+
+import co.edu.uniquindio.proyecto.servicios.intefaces.ClienteServicio;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/clientes")
+public class ClienteControlador {
+
+
+        private final ClienteServicio clienteServicio;
+
+
+        public void registrarCliente(){
+
+        }
+
+        public void actualizarCliente(){
+
+        }
+
+        public void eliminarCliente(){
+
+        }
+
+        public void listarClientes(){
+
+        }
+
+        public void buscarCliente(){
+
+        }
+
+        public void iniciarSesion(){
+
+        }
+
+        public void cerrarSesion(){
+
+        }
+
+        public void listarProductos(){
+
+        }
+
+        public void listarCategorias(){
+
+        }
+
+        public void listarMarcas(){
+
+        }
+
+        public void listarProductosPorCategoria(){
+
+        }
+
+        public void listarProductosPorMarca(){
+
+        }
+
+        public void listarProductosPorNombre(){
+
+        }
+
+        public void listarProductosPorPrecio(){
+
+        }
+
+        public void listarProductosPorDescuento(){
+
+        }
+
+        public void listarProductosPorCalificacion(){
+
+        }
+
+        public void listarProductosPorVendedor(){
+
+        }
+
+        public void listarProductosPorFecha(){
+
+        }
+
+        public void listarProductosPorEstado(){
+
+        }
+
+        public void listarProductosPorCantidad(){
+
+        }
+
+        public void listarProductosPorCalidad(){
+
+        }
+
+        public void listarProductosPorGarantia(){
+
+        }
+
+        public void listarProductosPorUbicacion(){
+
+        }
+
+        public void listarProductosPorEnvio(){
+
+        }
+
+        public void listarProductosPorDescuentoYEnvio(){
+
+        }
+
+        public void listarProductosPorDescuentoYUbicacion(){
+
+        }
+
+        public void listarProductosPorUbicacionYEnvio(){
+
+        }
+
+        public void listarProductosPorDescuentoUbicacionYEnvio(){
+
+        }
+
+        public void listarProductosPorCalificacionYVendedor(){
+
+        }
+
+        public void listarProductosPorCalificacionYFecha(){
+
+        }
+
+        public void listarProductosPorCalificacionYEstado(){
+
+        }
+
+        public void listarProductosPorCalificacionYCantidad(){
+
+        }
+
+        public void listarProductosPorCalificacionYCalidad(){
+
+        }
+
+        public void listarProductosPorCalificacionYGarantia(){
+
+        }
+
+        public void listarProductosPorCalificacionYUbicacion(){
+
+        }
+
+        public void listarProductosPorCalificacionYEnvio(){
+
+        }
+
+        public void listarProductosPorCalificacionYDescuento(){
+
+        }
+
+        public void listarProductosPorCalificacionYDescuentoYEnvio(){
+
+        }
+}
