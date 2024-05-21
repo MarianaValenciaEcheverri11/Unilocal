071ffe41705f3ac98feec564c8674834a22a3d06
David Velasco <David.santiago.velasco.12@gmail.com>
2024-05-21 10:55:24 -0500

    Arreglos varios

--- src/main/java/co/edu/uniquindio/proyecto/dto/ActualizacionCuentaDTO.java
+++ src/main/java/co/edu/uniquindio/proyecto/dto/ActualizacionCuentaDTO.java
@@ -7,6 +7,7 @@ import org.hibernate.validator.constraints.Length;
 public record ActualizacionCuentaDTO(
         @NotBlank String codigo,
         @NotBlank @Length(max = 100) String nombre,
+        @NotBlank @Length(max = 100) String nickName,
         @NotBlank String foto,
         @NotBlank @Email @Length(max = 100)  String email,
         @NotBlank @Length(max = 50) String ciudadResidencia
